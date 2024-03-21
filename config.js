dc0f920b6670d88ca078e2c2d3788e1dd4b02816
buitrongduong2001 <86511547+buitrongduong2001@users.noreply.github.com>
2024-03-21 10:20:19 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,23 @@
+// 霸都丶傲天 2019年12月24日
+
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "Công chúa yêu dấu của anh ơi!",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Anh muốn nói là...",  // 同上...
+        "Chúc người anh yêu một buổi chiều....",
+        "Thật nhiều niềm vui",
+        "Hạnh phúc",
+        "An nhiên",
+        "Vui vẻ",
+        "Và đặc biệt là cười thật nhiều em nhé ^-^ ",
+        "Anh ở đây và luôn yêu em &hearts; &hearts;",
+        "我非常爱你",
+        "YAMI~~",
+    ],
+    wish: "Gửi công chúa xinh đẹp của anh &hearts; &hearts;",  //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+    // 时间的格式很重要哦，一定要是下面这个格式!!
+    // 年年年年-月月-日日 时时:分分:秒秒
+    time: "2024-03-21 0:0:0",    //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+};
